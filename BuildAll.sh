32937652e76fd8d3a9a48b52167a6156fa64726c
Joeri van Dooren <ure@moreorless.be>
2016-02-13 10:13:37 +0100

    added memSQL to buildscript

--- BuildAll.sh
+++ BuildAll.sh
@@ -15,12 +15,14 @@ oc delete -f ImageStream-Proxy-Server.yaml
 oc delete -f BuildConfig-Proxy-Server.yaml
 oc delete -f ImageStream-WordPress-PHP-WebApp.yaml
 oc delete -f BuildConfig-WordPress-PHP-WebApp.yaml
-oc delete -f ImageStream-WordPress-HHVM-WebApp.yaml
-oc delete -f BuildConfig-WordPress-HHVM-WebApp.yaml
-oc delete -f ImageStream-PHP-Phalcon-Webserver.yaml
+oc delete -f ImageStream-WordPressoc login --token=vXLfgh_hs2adL91aPxvD-j35t8CVOtcb4GNyqMtxgjE --server=https://alpha.openshift.weepee.net:8443
+on-Webserver.yaml
 oc delete -f BuildConfig-PHP-Phalcon-Webserver.yaml
-oc delete -f ImageStream-memSQL-server.yaml
-oc delete -f BuildConfig-memSQL-server.yaml
+oc delete -f ImageStream-memSQL-Master-server.yaml
+oc delete -f BuildConfig-memSQL-Master-server.yaml
+oc delete -f ImageStream-memSQL-Node-server.yaml
+oc delete -f BuildConfig-memSQL-Node-server.yaml
+
 
 # Install
 oc create -f ImageStream-Static-Webserver.yaml
@@ -39,5 +41,7 @@ oc create -f ImageStream-WordPress-HHVM-WebApp.yaml
 oc create -f BuildConfig-WordPress-HHVM-WebApp.yaml
 oc create -f ImageStream-PHP-Phalcon-Webserver.yaml
 oc create -f BuildConfig-PHP-Phalcon-Webserver.yaml
-oc create -f ImageStream-memSQL-server.yaml
-oc create -f BuildConfig-memSQL-server.yaml
+oc create -f ImageStream-memSQL-Master-server.yaml
+oc create -f BuildConfig-memSQL-Master-server.yaml
+oc create -f ImageStream-memSQL-Node-server.yaml
+oc create -f BuildConfig-memSQL-Node-server.yaml
