eb0abffbe6f7600c2533f62c8039d868a11bb06d
Joeri van Dooren <ure@moreorless.be>
2016-02-16 10:07:01 +0100

    10 apps now ;)

--- BuildAll.sh
+++ BuildAll.sh
@@ -15,11 +15,13 @@ oc delete -f ImageStream-Proxy-Server.yaml
 oc delete -f BuildConfig-Proxy-Server.yaml
 oc delete -f ImageStream-WordPress-PHP-WebApp.yaml
 oc delete -f BuildConfig-WordPress-PHP-WebApp.yaml
-oc delete -f ImageStream-WordPressoc login --token=vXLfgh_hs2adL91aPxvD-j35t8CVOtcb4GNyqMtxgjE --server=https://alpha.openshift.weepee.net:8443
-on-Webserver.yaml
+oc delete -f ImageStream-PHP-Phalcon-Webserver.yaml
 oc delete -f BuildConfig-PHP-Phalcon-Webserver.yaml
 oc delete -f ImageStream-memSQL-server.yaml
 oc delete -f BuildConfig-memSQL-server.yaml
+oc delete -f ImageStream-phpPgAdmin-WebApp.yaml
+oc delete -f BuildConfig-phpPgAdmin-WebApp.yaml
+
 
 # Install
 oc create -f ImageStream-Static-Webserver.yaml
@@ -40,3 +42,5 @@ oc create -f ImageStream-PHP-Phalcon-Webserver.yaml
 oc create -f BuildConfig-PHP-Phalcon-Webserver.yaml
 oc create -f ImageStream-memSQL-server.yaml
 oc create -f BuildConfig-memSQL-server.yaml
+oc create -f ImageStream-phpPgAdmin-WebApp.yaml
+oc create -f BuildConfig-phpPgAdmin-WebApp.yaml
