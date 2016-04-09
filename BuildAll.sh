d42ef6e2235ee2d17f02cf48665c4636add0f763
Joeri van Dooren <ure@moreorless.be>
2016-04-09 20:06:51 +0200

    add redis

--- BuildAll.sh
+++ BuildAll.sh
@@ -6,28 +6,6 @@ oc new-project weepee-registry \
     --description="Weepee docker registry" \
     --display-name="Weepee Registry"
 
-
-# Cleanup
-oc delete -f ImageStream-Static-Webserver.yaml
-oc delete -f BuildConfig-Static-Webserver.yaml
-oc delete -f ImageStream-PHP-Webserver.yaml
-oc delete -f BuildConfig-PHP-Webserver.yaml
-oc delete -f ImageStream-Mojo-Webserver.yaml
-oc delete -f BuildConfig-Mojo-Webserver.yaml
-oc delete -f ImageStream-HHVM-Webserver.yaml
-oc delete -f BuildConfig-HHVM-Webserver.yaml
-oc delete -f ImageStream-Proxy-Server.yaml
-oc delete -f BuildConfig-Proxy-Server.yaml
-oc delete -f ImageStream-WordPress-PHP-WebApp.yaml
-oc delete -f BuildConfig-WordPress-PHP-WebApp.yaml
-oc delete -f ImageStream-PHP-Phalcon-Webserver.yaml
-oc delete -f BuildConfig-PHP-Phalcon-Webserver.yaml
-oc delete -f ImageStream-memSQL-server.yaml
-oc delete -f BuildConfig-memSQL-server.yaml
-oc delete -f ImageStream-phpPgAdmin-WebApp.yaml
-oc delete -f BuildConfig-phpPgAdmin-WebApp.yaml
-
-
 # Install
 oc create -f ImageStream-Static-Webserver.yaml
 oc create -f BuildConfig-Static-Webserver.yaml
@@ -53,3 +31,5 @@ oc create -f ImageStream-phpPgAdmin-WebApp.yaml
 oc create -f BuildConfig-phpPgAdmin-WebApp.yaml
 oc create -f ImageStream-Asterisk.yaml
 oc create -f BuildConfig-Asterisk.yaml
+oc create -f ImageStream-Redis.yaml
+oc create -f BuildConfig-Redis.yaml
